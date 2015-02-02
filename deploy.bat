c21068b94c0ce53bbe3bfabe8c6cfd0c45a28a48
Antoine Gersant <antoine.gersant@lesforges.org>
2015-02-01 23:31:44 -0800

    Include highcharts map library

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,7 @@ copy rtp\display.html bin\DBToDisplay\display.html /y
 copy rtp\style.css bin\DBToDisplay\style.css /y
 copy lib\highcharts\standalone-framework.js bin\DBToDisplay\highcharts-standalone-framework.js /y
 copy lib\highcharts\highcharts.js bin\DBToDisplay\highcharts.js /y
+copy lib\highcharts\map.js bin\DBToDisplay\highcharts-map.js /y
+copy lib\highcharts\world.js bin\DBToDisplay\highcharts-world.js /y
 copy lib\highcharts\dark-unica.js bin\DBToDisplay\highcharts-dark-unica.js /y
 copy lib\sql.js\js\sql.js bin\DBToDisplay\sql.js /y
