cb068f4df3195786c6bd80a271e9c8a075947ba5
Antoine Gersant <antoine.gersant@lesforges.org>
2015-02-02 23:25:18 -0800

    Adds SQL request for tracks per genre per country

--- src/agersant/blaireau/analyze/Analyze.hx
+++ src/agersant/blaireau/analyze/Analyze.hx
@@ -33,7 +33,7 @@ class Analyze
 	public static function getTracksPerGenrePerYear(db : Database) : Array<{genre: String, year : Int, numTracks : Int}>
 	{
 		var result = db.exec('	SELECT Genre.name AS genre, Album.year as year, COUNT(*) as numTracks
-								FROM Track								
+								FROM Track
 								INNER JOIN Album ON Album.id = Track.albumID
 								INNER JOIN _join_Genre_Track ON _join_Genre_Track.r2 = Track.id
 								INNER JOIN Genre ON Genre.id = _join_Genre_Track.r1
@@ -46,10 +46,23 @@ class Analyze
 	public static function getTracksPerCountry(db : Database) : Array<{country: String, numTracks : Int}>
 	{
 		var result = db.exec('	SELECT Country.name AS country, COUNT(*) as numTracks
-								FROM Track								
+								FROM Track
 								INNER JOIN _join_Country_Track ON _join_Country_Track.r2 = Track.id
 								INNER JOIN Country ON Country.id = _join_Country_Track.r1
 								GROUP BY Country.id
+							');
+		return cast(mapSQLResult(result[0].columns, result[0].values));
+	}
+	
+	public static function getTracksPerGenrePerCountry(db : Database) : Array<{country: String, genre: String, numTracks : Int}>
+	{
+		var result = db.exec('	SELECT Country.name AS country, Genre.name AS genre, COUNT(*) AS numTracks
+								FROM Country
+								INNER JOIN _join_Country_Track ON _join_Country_Track.r1 = Country.id
+								INNER JOIN Track ON Track.id = _join_Country_Track.r2
+								INNER JOIN _join_Genre_Track ON _join_Genre_Track.r2 = Track.id
+								INNER JOIN Genre ON Genre.id = _join_Genre_Track.r1
+								GROUP BY Country.id, Genre.id
 							');
 		return cast(mapSQLResult(result[0].columns, result[0].values));
 	}
